6046068ed0ded0fd69e16ca4af0eb999db5234fb
facundoq <facundoq@gmail.com>
2021-08-05 02:43:06 -0300

    added device option

--- transformational_measures/pytorch/layer_measures.py
+++ transformational_measures/pytorch/layer_measures.py
@@ -10,8 +10,8 @@ class Variance(PyTorchLayerMeasure):
             for batch_n,batch_activations in enumerate(row_iterator):
                 row_variance.update_all(batch_activations.double())
             row_std = row_variance.std()
-            print(row_std.device)
             mean.update(row_std)
+        print(mean.mu.device)
         return mean.mean()
 
 

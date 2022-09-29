1f95fc80ce41d36f0d894301825011b8fe00f784
facundoq <facundoq@gmail.com>
2022-09-29 16:16:33 -0300

    from future annotations to support python <3.10

--- tmeasures/np/distance_same_equivariance.py
+++ tmeasures/np/distance_same_equivariance.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import List
 from .activations_iterator import ActivationsIterator
 from tmeasures import MeasureResult
--- tmeasures/visualization/layers.py
+++ tmeasures/visualization/layers.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import enum
 import tmeasures as tm
 import matplotlib.pyplot as plt

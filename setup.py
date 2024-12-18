b83dc2a567e710b91891ab41b1e1b05382e9bd06
facundoq <facundoq@gmail.com>
2024-12-18 09:28:19 -0300

    updated github actions

--- setup.py
+++ setup.py
@@ -15,43 +15,6 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 url="https://github.com/facundoq/tmeasures"
 VERSION='1.2.9'
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        dist_path=Path(this_directory)/'dist'
-        if dist_path.exists():
-            self.status('Removing previous builds…')
-            rmtree(dist_path)
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system(
-            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(VERSION))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
 setup(
     name="tmeasures",
     version=VERSION,
@@ -103,7 +66,4 @@ setup(
     # could also include long_description, download_url, etc.
     long_description=long_description,
     long_description_content_type='text/markdown',
-    # cmdclass={
-    #     'upload': UploadCommand,
-    # },
 )
